1c2e7acb93d5c7a4f3a1eb8e4a9b8e8b280a9fdf
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-07-07 00:51:17 +0530

    added : tour schema, model and document

--- server.js
+++ server.js
@@ -38,6 +38,54 @@ mongoose.connect(DB).then((connectionObj) => {
   console.log('DB connection successfull');
 });
 
+/**
+ * mongoose is all about models, and model is like a blueprint that we use to create documents, so it's like
+ * a classes in javascript, that we use as a blueprint to create objects.
+ *
+ * So in mongoose we create models in order to create documents using it, and to also perform CRUD operations.
+ *
+ * And in order to create a model we need a schema.
+ *
+ * We use schema to create model out of it, and we use schema to describe type of our data,
+ * to set default values, to validate the data etc.
+ *
+ */
+
+/**
+ * Schema
+ */
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A Tour must have a price'] },
+});
+
+/**
+ * Creating model out of schema
+ */
+const Tour = mongoose.model('Tour', tourSchema);
+
+/**
+ * Creating document from model
+ */
+const testTour = new Tour({
+  name: 'Test tour 11',
+  rating: 4.6,
+  price: 500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => [console.error('ERROR', err)]);
+
 const port = process.env.PORT || 5000;
 /**
  * Listening
