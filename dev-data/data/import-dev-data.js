7fd6d7c322af1769a6a025e82216f4f7a310b373
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-07-09 00:56:48 +0530

    added : script to add development data from cmd

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import Tour from '../../models/tourModel.js';
+
+dotenv.config({ path: './config.env' });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((connectionObj) => {
+  // console.log(connectionObj.connection);
+  console.log('DB connection successfull');
+});
+
+// ########### Read json file ###########
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// ########### Import data in database ###########
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// ########### Delete all data from collection ###########
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data succesfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
