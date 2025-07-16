fbf81ba35c22676ba8fc1f236bf7320204fff35c
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-07-17 01:29:06 +0530

    added : catching async functions errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 import Tour from '../models/tourModel.js';
 import APIFeatures from '../utils/apiFeatures.js';
+import catchAsyncError from '../utils/catchAsyncError.js';
 
 /**
  * Aliasing
@@ -195,96 +196,146 @@ export const getAllTours = async (req, res) => {
  * getTour : Route handler for getting a single tour
  */
 
-export const getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id);
-    // const tour = await Tour.findById(req.params.id);
+export const getTour = catchAsyncError(async (req, res) => {
+  const tour = await Tour.findById(req.params.id);
+  // const tour = await Tour.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: tour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+// export const getTour = async (req, res) => {
+//   try {
+//     const tour = await Tour.findById(req.params.id);
+//     // const tour = await Tour.findById(req.params.id);
+
+//     res.status(200).json({
+//       status: 'success',
+//       data: {
+//         tour: tour,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
+
+/**
+ * Catching error in async functions
+ */
 
 /**
  * createTour : Route handler for creating a tour
  */
 
-export const createTour = async (req, res) => {
-  try {
-    // ### In this method we call the save method on the document ###
-    // const newTour = new Tour({})
-    // newTour.save()
-    // ### we can also create a tour using .create() method directly on the Tour model ####
+export const createTour = catchAsyncError(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
 
-    const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+// export const createTour = async (req, res) => {
+//   try {
+//     // ### In this method we call the save method on the document ###
+//     // const newTour = new Tour({})
+//     // newTour.save()
+//     // ### we can also create a tour using .create() method directly on the Tour model ####
+
+//     const newTour = await Tour.create(req.body);
+
+//     res.status(201).json({
+//       status: 'success',
+//       data: {
+//         tour: newTour,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
 
 /**
  * updateTour : Route handler for updating a tour
  */
 
-export const updateTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+export const updateTour = catchAsyncError(async (req, res) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: tour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+// export const updateTour = async (req, res) => {
+//   try {
+//     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//       runValidators: true,
+//     });
+
+//     res.status(200).json({
+//       status: 'success',
+//       data: {
+//         tour: tour,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
 
 /**
  * deleteTour : Route handler for deleting a tour
  */
 
-export const deleteTour = async (req, res) => {
-  try {
-    await Tour.findByIdAndDelete(req.params.id);
+export const deleteTour = catchAsyncError(async (req, res) => {
+  await Tour.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+// export const deleteTour = async (req, res) => {
+//   try {
+//     await Tour.findByIdAndDelete(req.params.id);
+
+//     res.status(204).json({
+//       status: 'success',
+//       data: null,
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
 
 /**
  * Aggregation pipeline
