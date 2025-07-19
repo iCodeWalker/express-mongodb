28b488b1186ab8203b90c8945185a6e263f7ce82
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-07-19 23:08:03 +0530

    added : errors for development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,14 +6,39 @@
  * And express will automatically recognise it as an error handling middleware
  */
 
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('Error -->', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'something went wrong',
+    });
+  }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorForProd(err, res);
+  }
 };
 
 export default globalErrorHandler;
